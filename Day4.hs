1176b06683c7febd13a574937876743412cd312f
sondresl <sondreslunde@gmail.com>
2020-12-04 14:37:37 +0100

    Cool parser

--- Day4.hs
+++ Day4.hs
@@ -1,53 +1,69 @@
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ViewPatterns #-}
 {-# LANGUAGE NoImplicitPrelude #-}
 
 import qualified Data.Set as Set
+import qualified Data.Text as T
 import Relude hiding ((<|>))
 import Text.Parsec
 
+type Parser = Parsec Text ()
+
 main :: IO ()
 main = do
-  Right input <- parse parser "" <$> readFileText "./input/day4"
-  print $ length $ filter isValid input -- 204
-  print $ length $ filter isValid' input -- 179
+    input <- fetch pair
+    print $ length $ filter isValid input -- 204
 
-parser :: Parsec Text () [[(String, String)]]
-parser = pairs `sepBy` space
-  where
-    pairs = many1 $ pair <* space
-    pair = (,) <$> many1 letter <* char ':' <*> many1 (noneOf [' ', '\n'])
+    part2 <- fetch validPair
+    print $ length $ filter isValid part2 -- 179
+
+fetch :: Parser String -> IO [[String]]
+fetch p = mapMaybe (rightToMaybe . parse (parser p) "")
+            . T.splitOn "\n\n"
+            <$> readFileText "./input/day4"
+
+parser :: Parser String -> Parser [String]
+parser p = p `sepBy` space
+
+pair :: Parser String
+pair = many1 letter <* char ':' <* many1 (noneOf " \n")
 
 -- Part 1
-isValid :: [(String, String)] -> Bool
-isValid = Set.isSubsetOf required . Set.fromList . map fst
+isValid :: [String] -> Bool
+isValid = Set.isSubsetOf required . Set.fromList
   where
     required = Set.fromList ["ecl", "pid", "eyr", "hcl", "byr", "iyr", "hgt"]
 
--- Part 2
-isValid' :: [(String, String)] -> Bool
-isValid' xs = isValid xs && all validPair xs
+key :: String -> Parser String
+key s = try $ string s <* char ':'
 
-validPair :: (String, String) -> Bool
-validPair = \case
-  ("byr", readMaybe -> Just birthYear) -> inRange birthYear 1920 2002
-  ("iyr", readMaybe -> Just issueYear) -> inRange issueYear 2010 2020
-  ("eyr", readMaybe -> Just expirationYear) -> inRange expirationYear 2020 2030
-  ("hgt", parse heightP "" -> Right (Left (Just n))) -> inRange n 150 193
-  ("hgt", parse heightP "" -> Right (Right (Just n))) -> inRange n 59 76
-  ("hcl", x) -> parses (char '#' <* replicateM 6 (digit <|> oneOf ['a' .. 'f'])) x
-  ("ecl", x) -> parses (choice $ try . string <$> ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]) x
-  ("pid", x) -> parses (replicateM 9 digit) x
-  ("cid", _) -> True
-  _ -> False
+validPair :: Parser String
+validPair = choice
+    [ key "byr" <* validRange 1920 2002
+    , key "iyr" <* validRange 2010 2020
+    , key "eyr" <* validRange 2020 2030
+    , key "hgt" <* heightP
+    , key "hcl" <* (char '#' >> replicateM 6 (digit <|> oneOf ['a' .. 'f']))
+    , key "ecl" <* choice (try . string <$> ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"])
+    , key "pid" <* replicateM 9 digit
+    , key "cid" <* many1 (noneOf " \n")
+    ]
 
 inRange :: Ord a => a -> a -> a -> Bool
 inRange x l u = x >= l && x <= u
 
+validRange :: Int -> Int -> Parser ()
+validRange i j = do
+  Just x <- readMaybe <$> many1 digit
+  guard $ inRange x i j
+
 parses :: Parsec String () a -> String -> Bool
 parses p x = isRight $ parse (p <* eof) "" x
 
-heightP :: Parsec String () (Either (Maybe Integer) (Maybe Integer))
+heightP :: Parser ()
 heightP = do
-  number <- readMaybe <$> many1 digit
-  string "cm" $> Left number <|> string "in" $> Right number
+    Just number <- readMaybe <$> many1 digit
+    choice
+      [ string "cm" *> guard (inRange number 150 193)
+      , string "in" *> guard (inRange number 59 76) ]
