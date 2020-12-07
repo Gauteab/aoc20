e65f0b84d208bf4a35b8f2a9f828eca09cbc3e2c
Gaute Berge <gaute.berge@gmail.com>
2020-12-07 09:35:33 +0100

    day7

---
+++ Day7.hs
@@ -0,0 +1,46 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ViewPatterns #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+
+import Data.List (nub)
+import qualified Data.Map as Map
+import Data.Maybe (fromJust)
+import Relude hiding (optional)
+import Text.Parsec
+
+main :: IO ()
+main = do
+  Right input <- traverse (parse parser "") . lines <$> readFileText "./input/day7"
+  print $ part1 input -- 208
+  print $ part2 input -- 1664
+
+type Entry = (String, [(Int, String)])
+
+parser :: Parsec Text () Entry
+parser = (,) <$> bag <* string " bags contain " <*> declaration `sepBy` string ", "
+  where
+    bag = (<>) <$> word <* space <*> word
+    declaration = (,) <$> int <* space <*> bag <* string " bag" <* optional (char 's')
+    int = fromJust . readMaybe <$> many1 digit
+    word = many1 letter
+
+hasBag :: String -> Entry -> Bool
+hasBag s (_, bags) = s `elem` map snd bags
+
+part1 :: [Entry] -> Int
+part1 input = length . nub . concatMap (map fst) . takeWhile (not . null) $ iterate f initial
+  where
+    initial = filter (hasBag "shinygold") input
+    f = concatMap $ \(name, _) -> filter (hasBag name) input
+
+part2 :: [Entry] -> Int
+part2 (Map.fromList -> input) = subtract 1 . fromJust $ go "shinygold"
+  where
+    go :: String -> Maybe Int
+    go bag = do
+      xs <- Map.lookup bag input
+      case xs of
+        [] -> pure 1
+        _ -> do
+          products <- traverse (\(n, name) -> (* n) <$> go name) xs
+          pure $ 1 + sum products
