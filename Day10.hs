489ef7f959e5b27e7feed7dc464fa8dadd01ebe7
Gaute Berge <gaute.berge@gmail.com>
2020-12-10 13:14:12 +0100

    fixes

--- Day10.hs
+++ Day10.hs
@@ -11,15 +11,11 @@ main :: IO ()
 main = do
   Just input <- traverse (readMaybe @Int . toString) . lines <$> readFileText "./input/day10"
   let input' = 0 : sort input <> [maximum input + 3]
-      differences = map (uncurry (flip (-))) $ (zip <*> Prelude.tail) input'
+      differences = map (uncurry subtract) $ (zip <*> Prelude.tail) input'
   -- Part 1: (1856)
   print . product . map length . group . sort $ differences
   -- Part 2: (2314037239808)
   print . product . map (possibilities . length) . filter (any (/= 3)) $ group differences
 
 possibilities :: Int -> Int
-possibilities 1 = 1
-possibilities 2 = 2
-possibilities 3 = 4
-possibilities 4 = 7
-possibilities _ = error "I don't know man"
+possibilities n = n * (n - 1) `div` 2 + 1
